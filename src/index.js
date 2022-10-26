37030c0d0ded011099379b89ddc60a279cb03636
djbenjamin2001 <100559479+djbenjamin2001@users.noreply.github.com>
2022-10-26 12:27:00 +0200

    Revert "Dogus auto"

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import section from "./components/section.js"
-import hero from "./components/hero.js"
- let element = document.querySelector('.wrapper')
-
-
-element.append(hero())
-element.append(section())
